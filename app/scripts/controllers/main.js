47d4947c2925a3c1c42961b007d52c5c699221c1
Ashik <ashikes@gmail.com>
2014-03-25 19:29:03 +0530

    adding project template files.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngDragDropApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

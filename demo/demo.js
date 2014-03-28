dc7191d46367646ae06a74e0bf87e34b071e9d6e
Ashik <ashikes@gmail.com>
2014-03-28 19:49:32 +0530

    Modified some style for demo app.

--- demo/demo.js
+++ demo/demo.js
@@ -7,7 +7,7 @@
 
             $scope.board = {"name":"Stuff to do at home","numberOfColumns":4,
                 "columns":[
-                    {"name":"Not started","cards":[{"name":"Come up with a POC for new Project"},{"name":"Design new framework for reporting module"}]},
+                    {"name":"Ideas","cards":[{"name":"Come up with a POC for new Project"},{"name":"Design new framework for reporting module"}]},
                     {"name":"Not started","cards":[{"name":"Explore new IDE for Development"},{"name":"Get new resource for new Project"}]},
                     {"name":"In progress","cards":[{"name":"Develop ui for tracker module"},{"name":"Develop backend for plan module"}]},
                     {"name":"Done","cards":[{"name":"Test user module"},{"name":"End to End Testing for user group module"},{"name":"CI for user module"}]}]};

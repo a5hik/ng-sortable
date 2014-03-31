151c1df8e9251a379b72a0208aa4235599efe432
Ashik <ashikes@gmail.com>
2014-03-31 19:12:11 +0530

    Added Callback methods for sortable directive.

--- demo/demo.js
+++ demo/demo.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular.module('demoApp', ['ui.sortable'])
-        .controller('demoController', function($scope) {
+        .controller('demoController', function($scope, $log) {
 
             $scope.board = {"name":"Stuff to do at home","numberOfColumns":4,
                 "columns":[
@@ -11,5 +11,12 @@
                     {"name":"Not started","cards":[{"name":"Explore new IDE for Development"},{"name":"Get new resource for new Project"}]},
                     {"name":"In progress","cards":[{"name":"Develop ui for tracker module"},{"name":"Develop backend for plan module"}]},
                     {"name":"Done","cards":[{"name":"Test user module"},{"name":"End to End Testing for user group module"},{"name":"CI for user module"}]}]};
+
+            $scope.sortOptions = {
+
+                itemClicked: function (sourceItem) {
+                 $log.info(sourceItem.title);
+                }
+            };
         });
 })();
--- source/sortable.js
+++ source/sortable.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     'use strict';
@@ -8,22 +7,103 @@
      * Controller for Sortable.
      * @param $scope
      */
-    function sortableController($scope) {
+    mainModule.controller('sortableController', ['$scope', function($scope) {
 
-    }
-    sortableController.$inject = ['$scope'];
+        $scope.sortableElement = null;
+        $scope.sortableModelValue = null;
+        $scope.callbacks = null;
+        $scope.items = [];
+
+        $scope.initSortable = function (element) {
+            $scope.sortableElement = element;
+        };
+
+    }]);
 
     mainModule.directive('sortable', [
-            function () {
-                return {
-                    require: [],
-                    restrict: 'A',
-                    scope: true,
-                    controller: 'sortableController',
-                    link: function (scope, element, attrs) {
+        function () {
+            return {
+                require: ['ngModel'], // get a hold of NgModelController
+                restrict: 'A',
+                scope: true,
+                controller: 'sortableController',
+                link: function (scope, element, attrs, controllersArr) {
 
-                    }
-                };
-            }]);
+                    var callbacks = {
+                        accept: null,
+                        orderChanged: null,
+                        itemRemoved: null,
+                        itemAdded: null,
+                        itemMoved: null,
+                        itemClicked: null,
+                        start: null,
+                        move: null,
+                        stop: null
+                    };
+
+                    var ngModel = controllersArr[0];
+
+                    scope.initSortable(element);
+
+                    if (!ngModel) return; // do nothing if no ng-model
+
+                    // Specify how UI should be updated
+                    ngModel.$render = function () {
+                        //set the model value in scope.
+                        scope.sortableModelValue = ngModel.$modelValue;
+                    };
+
+                    callbacks.accept = function (modelData, sourceItemScope, targetScope, destIndex) {
+                        return true;
+                    };
+
+                    callbacks.orderChanged = function (scope, sourceItem, sourceIndex, destIndex) {
+
+                    };
+
+                    callbacks.itemRemoved = function (scope, sourceItem, sourceIndex) {
+
+                    };
+
+                    callbacks.itemAdded = function (scope, sourceItem, destIndex) {
+
+                    };
+
+                    callbacks.itemMoved = function (sourceScope, sourceItem, sourceIndex, destScope, destIndex) {
+
+                    };
+
+                    callbacks.itemClicked = function (sourceItem) {
+
+                    };
+
+                    callbacks.start = function (scope, sourceItem, elements) {
+
+                    };
+
+                    callbacks.move = function (scope, sourceItem, elements) {
+
+                    };
+
+                    callbacks.stop = function (scope, sourceItem, elements) {
+
+                    };
+
+                    // When we add or remove elements, we need the sortable to 'refresh'
+                    //Compare by value not by reference, by the last set to true.
+                    scope.$watch(attrs.sortable, function (newVal, oldVal) {
+                        angular.forEach(newVal, function (value, key) {
+                            if (callbacks[key]) {
+                                if (typeof value === 'function') {
+                                    callbacks[key] = value;
+                                }
+                            }
+                        });
+                        scope.callbacks = callbacks;
+                    }, true);
+
+                }
+            };
+        }]);
 
 })();

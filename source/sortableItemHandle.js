1655761b71d1eb4c3398601c5a187068e72517fe
Ashik <ashikes@gmail.com>
2014-04-01 15:47:12 +0530

    Added drag mouse event methods.

--- source/sortableItemHandle.js
+++ source/sortableItemHandle.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     'use strict';
@@ -8,42 +7,70 @@
      * Controller for sortableItemHandle
      * @param $scope
      */
-    mainModule.controller('sortableItemHandleController', ['$scope', function($scope) {
+    mainModule.controller('sortableItemHandleController', ['$scope', function ($scope) {
 
-        $scope.initHandle = function(element) {
+        $scope.initHandle = function (element) {
             element.attr('sortable-elment-type', 'handle');
         };
 
     }]);
 
     mainModule.directive('sortableItemHandle', ['$window', '$document',
-            function ($window, $document) {
-                return {
-                    require: ['^sortableItem'],
-                    restrict: 'A',
-                    controller: 'sortableItemHandleController',
-                    link: function (scope, element, attrs, itemController) {
+        function ($window, $document) {
+            return {
+                require: ['^sortableItem'],
+                restrict: 'A',
+                controller: 'sortableItemHandleController',
+                link: function (scope, element, attrs, itemController) {
 
-                        var clickedElm, sourceItem;
+                    var clickedElm, sourceItem;
 
-                        scope.initHandle(element);
+                    scope.initHandle(element);
 
-                        var dragStartEvent = function(e) {
-                            clickedElm = angular.element(e.target);
-                            sourceItem = clickedElm.scope().itemData();
+                    var hasTouch = 'ontouchstart' in window;
 
-                            e.preventDefault();
-                        };
+                    var dragStartEvent = function (event) {
+                        clickedElm = angular.element(event.target);
+                        sourceItem = clickedElm.scope().itemData();
 
-                        var dragMoveEvent = function(e) {
+                        event.preventDefault();
 
-                        };
-
-                        var dragEndEvent = function(e) {
-
+                        if (hasTouch) {
+                            angular.element($document).bind('touchmove', dragMoveEvent);
+                            angular.element($document).bind('touchend', dragEndEvent);
+                            angular.element($document).bind('touchcancel', dragEndEvent);
+                        } else {
+                            angular.element($document).bind('mousemove', dragMoveEvent);
+                            angular.element($document).bind('mouseup', dragEndEvent);
                         }
+                    };
+
+                    var dragMoveEvent = function (event) {
+
+                    };
+
+                    var dragEndEvent = function (event) {
+
+                        if (hasTouch) {
+                            angular.element($document).unbind('touchend', dragEndEvent);
+                            angular.element($document).unbind('touchcancel', dragEndEvent);
+                            angular.element($document).unbind('touchmove', dragMoveEvent);
+                        }
+                        else {
+                            angular.element($document).unbind('mouseup', dragEndEvent);
+                            angular.element($document).unbind('mousemove', dragMoveEvent);
+                            angular.element($window.document.body).unbind('mouseleave', dragEndEvent);
+                        }
+
+                    };
+
+                    if (hasTouch) {
+                        element.bind('touchstart', dragStartEvent);
+                    } else {
+                        element.bind('mousedown', dragStartEvent);
                     }
-                };
-            }]);
+                }
+            };
+        }]);
 
 })();

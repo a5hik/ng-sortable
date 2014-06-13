bb8037d88ed73ba1241fa0fb84f65013d4b41cce
Ashik <ashikes@gmail.com>
2014-06-13 16:24:24 +0530

    added example: restrict move cards across columns.

--- demo/model.js
+++ demo/model.js
@@ -1,22 +1,25 @@
+/*jshint undef: false, unused: false, indent: 2*/
+/*global angular: false */
+
 'use strict';
 function Board(name, numberOfColumns) {
-    return {
-        name: name,
-        numberOfColumns: numberOfColumns,
-        columns: []
-    };
+  return {
+    name: name,
+    numberOfColumns: numberOfColumns,
+    columns: []
+  };
 }
 
 function Column(name) {
-    return {
-        name: name,
-        cards: []
-    };
+  return {
+    name: name,
+    cards: []
+  };
 }
 
 function Card(title, status, details) {
-    this.title = title;
-    this.status = status;
-    this.details = details;
-    return this;
+  this.title = title;
+  this.status = status;
+  this.details = details;
+  return this;
 }

915591952127b7ba067e2e06721b70cb7b4bd6d9
a5hik <ashikes@gmail.com>
2014-12-20 12:57:26 +0530

    updating gh-pages

--- bower_components/angular-route/angular-route.js
+++ bower_components/angular-route/angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.2
+ * @license AngularJS v1.3.8
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -41,7 +41,7 @@ var ngRouteModule = angular.module('ngRoute', ['ng']).
  */
 function $RouteProvider() {
   function inherit(parent, extra) {
-    return angular.extend(new (angular.extend(function() {}, {prototype:parent}))(), extra);
+    return angular.extend(Object.create(parent), extra);
   }
 
   var routes = {};
@@ -151,6 +151,9 @@ function $RouteProvider() {
     if (angular.isUndefined(routeCopy.reloadOnSearch)) {
       routeCopy.reloadOnSearch = true;
     }
+    if (angular.isUndefined(routeCopy.caseInsensitiveMatch)) {
+      routeCopy.caseInsensitiveMatch = this.caseInsensitiveMatch;
+    }
     routes[path] = angular.extend(
       routeCopy,
       path && pathRegExp(path, routeCopy)
@@ -158,9 +161,9 @@ function $RouteProvider() {
 
     // create redirection for trailing slashes
     if (path) {
-      var redirectPath = (path[path.length-1] == '/')
-            ? path.substr(0, path.length-1)
-            : path +'/';
+      var redirectPath = (path[path.length - 1] == '/')
+            ? path.substr(0, path.length - 1)
+            : path + '/';
 
       routes[redirectPath] = angular.extend(
         {redirectTo: path},
@@ -170,6 +173,17 @@ function $RouteProvider() {
 
     return this;
   };
+
+  /**
+   * @ngdoc property
+   * @name $routeProvider#caseInsensitiveMatch
+   * @description
+   *
+   * A boolean property indicating if routes defined
+   * using this provider should be matched using a case insensitive
+   * algorithm. Defaults to `false`.
+   */
+  this.caseInsensitiveMatch = false;
 
    /**
     * @param path {string} path
@@ -639,11 +653,11 @@ function $RouteProvider() {
      */
     function interpolate(string, params) {
       var result = [];
-      angular.forEach((string||'').split(':'), function(segment, i) {
+      angular.forEach((string || '').split(':'), function(segment, i) {
         if (i === 0) {
           result.push(segment);
         } else {
-          var segmentMatch = segment.match(/(\w+)(.*)/);
+          var segmentMatch = segment.match(/(\w+)(?:[?*])?(.*)/);
           var key = segmentMatch[1];
           result.push(params[key]);
           result.push(segmentMatch[2] || '');
@@ -774,7 +788,6 @@ ngRouteModule.directive('ngView', ngViewFillContentFactory);
         .view-animate-container {
           position:relative;
           height:100px!important;
-          position:relative;
           background:white;
           border:1px solid black;
           height:40px;
--- dist/ng-sortable.js
+++ dist/ng-sortable.js
@@ -27,7 +27,7 @@
 
 (function () {
   'use strict';
-  angular.module('ui.sortable', [])
+  angular.module('as.sortable', [])
     .constant('sortableConfig', {
       itemClass: 'as-sortable-item',
       handleClass: 'as-sortable-item-handle',
@@ -43,7 +43,7 @@
 (function () {
   'use strict';
 
-  var mainModule = angular.module('ui.sortable');
+  var mainModule = angular.module('as.sortable');
 
   /**
    * Helper factory for sortable.
@@ -315,7 +315,7 @@
 (function () {
 
   'use strict';
-  var mainModule = angular.module('ui.sortable');
+  var mainModule = angular.module('as.sortable');
 
   /**
    * Controller for Sortable.
@@ -329,6 +329,7 @@
     $scope.callbacks = null;
     $scope.type = 'sortable';
     $scope.options = {};
+    $scope.isDisabled = false;
 
     /**
      * Inserts the item in to the sortable list.
@@ -498,10 +499,14 @@
             scope.callbacks = callbacks;
           }, true);
 
-          // Set isEnabled if attr is set, if undefined isEnabled = true
-          scope.$watch(attrs.isEnabled, function (newVal, oldVal) {
-            scope.isEnabled = newVal !== undefined ? newVal : true;
-          }, true);
+          // Set isDisabled if attr is set, if undefined isDisabled = false
+          if(angular.isDefined(attrs.isDisabled)) {
+            scope.$watch(attrs.isDisabled, function (newVal, oldVal) {
+              if(!angular.isUndefined(newVal)) {
+                scope.isDisabled = newVal;
+              }
+            }, true);
+          }
         }
       };
     });
@@ -514,7 +519,7 @@
 (function () {
 
   'use strict';
-  var mainModule = angular.module('ui.sortable');
+  var mainModule = angular.module('as.sortable');
 
   /**
    * Controller for sortableItemHandle
@@ -563,7 +568,7 @@
             unBindEvents,//unbind the drag events.
             hasTouch,// has touch support.
             dragHandled, //drag handled.
-            isEnabled = true; //sortable is enabled
+            isDisabled = false; // drag enabled
 
           hasTouch = $window.hasOwnProperty('ontouchstart');
 
@@ -573,18 +578,16 @@
 
           scope.itemScope = itemController.scope;
 
-          scope.$watch('sortableScope.isEnabled', function(newVal) {
-            if (isEnabled !== newVal) {
-              isEnabled = newVal;
-
-              if (isEnabled) {
-                bindDrag();
-              } else {
+          scope.$watch('sortableScope.isDisabled', function(newVal) {
+            if(isDisabled !== newVal) {
+              isDisabled = newVal;
+              if(isDisabled) {
                 unbindDrag();
+              } else {
+                bindDrag();
               }
             }
           });
-
 
           /**
           * Listens for a 10px movement before
@@ -706,10 +709,17 @@
 
             elementClicked = angular.element(event.target);
             sourceScope = elementClicked.scope();
-            isDraggable = true;
-            if (!sourceScope || !sourceScope.type || sourceScope.type !== 'handle') {
-              return false;
+
+            // look for the handle on the current scope or parent scopes
+            isDraggable = false;
+            while (!isDraggable && sourceScope !== undefined) {
+              if (sourceScope.type && sourceScope.type === 'handle') {
+                isDraggable = true;
+              } else {
+                sourceScope = sourceScope.$parent;
+              }
             }
+
             //If a 'no-drag' element inside item-handle if any.
             while (isDraggable && elementClicked[0] !== element[0]) {
               if ($helper.noDrag(elementClicked)) {
@@ -987,7 +997,7 @@
 (function () {
 
   'use strict';
-  var mainModule = angular.module('ui.sortable');
+  var mainModule = angular.module('as.sortable');
 
   /**
    * Controller for sortable item.

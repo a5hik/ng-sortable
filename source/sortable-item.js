10942f06b6324c3b4a1a756ea8b5220e7da38c2d
a5hik <ashikes@gmail.com>
2015-04-04 19:34:57 +0530

    Pushing the PR for #120

--- source/sortable-item.js
+++ source/sortable-item.js
@@ -25,7 +25,7 @@
      * @returns {*} - index value.
      */
     $scope.index = function () {
-      return $scope.sortableScope.modelValue.indexOf($scope.modelValue);
+      return $scope.$index;
     };
 
     /**

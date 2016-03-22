ab9aeb978504c86823f43f1ecc22e64d13ca7d94
Ben Arunski <ben.arunski@capstonec.com>
2016-03-22 11:43:28 -0500

    fix for #281 and #285

--- source/sortable-item-handle.js
+++ source/sortable-item-handle.js
@@ -539,9 +539,8 @@
               } else {
                 element.bind('touchstart', dragListen);
               }
-            } else {
-              element.bind('mousedown', dragListen);
             }
+            element.bind('mousedown', dragListen);
           };
 
           /**
